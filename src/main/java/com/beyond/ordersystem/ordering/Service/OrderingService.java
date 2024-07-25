5f4c5b2c0fe04e3dec92071817584d1d7c0ddee3
wisdom <wisdom0405@kakao.com>
2024-07-25 23:34:15 +0900

    spring 주문시스템n:m관계 풀어가기

--- src/main/java/com/beyond/ordersystem/ordering/Service/OrderingService.java
+++ src/main/java/com/beyond/ordersystem/ordering/Service/OrderingService.java
@@ -39,20 +39,20 @@ public class OrderingService {
                 .orderDetails(new ArrayList<>()) // 아직 아무것도 안들어간 orderDetail 리스트
                 .build();
 
-        for (OrderingSaveReqDto.OrderDetailDto orderDetailDto : dto.getOrderDetailDtoList()){
+        for (OrderingSaveReqDto.OrderDetailDto orderDetailDto : dto.getOrderDetailDtoList()){ // OrderingSaveReqDto의 orderDetailDto리스트 요소 하나씩 꺼내옴
             // OrderDetailDto에 딸린 productId를 가지고 product 객체 찾음
             Product product = productRepository.findById(orderDetailDto.getProductId()).orElseThrow(()->new EntityNotFoundException("product is not found"));
 
-            OrderDetail orderDetail = OrderDetail.builder()
+            OrderDetail orderDetail = OrderDetail.builder() // 주문상세 OrderDetail 객체 조립
                     .product(product)
                     .ordering(ordering)
                     .quantity(orderDetailDto.getProductCount())
                     .build();
-            ordering.getOrderDetails().add(orderDetail);
+            ordering.getOrderDetails().add(orderDetail); // 여기서 이제 orderDetails 리스트에 orderDetail 하나씩 차례로 add해준다.
         }
 
-        Ordering savedOrdering = orderingRepository.save(ordering);
-
+        Ordering savedOrdering = orderingRepository.save(ordering); // 여기서 save해줘도 jpa에 의해서 선후관계 알아서 맞춰서 처리해주기 때문에 코드의 선후관계 안따져도 OK
+        // 이제 Ordering 객체를 save 해줬으므로 Ordering 객체의 id값 나올 것 -> 이후 필요한 요소 jpa가 알아서 처리해준다.
         return savedOrdering;
     }
 }
